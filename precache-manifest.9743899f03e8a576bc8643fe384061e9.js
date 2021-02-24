ef26b3fbf52d99886b89f0d4627ca83650126d61
SonnyFishback <sonny.fishback7@gmail.com>
2021-02-24 15:27:33 -0500

    Updates

---
+++ precache-manifest.9743899f03e8a576bc8643fe384061e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb1889d56b82455cec0b6cce6420676",
+    "url": "/popular-film-listings/index.html"
+  },
+  {
+    "revision": "b69ebe7545e501d0d321",
+    "url": "/popular-film-listings/static/css/main.a1ad1b68.chunk.css"
+  },
+  {
+    "revision": "0abbe97b761b41e38a2b",
+    "url": "/popular-film-listings/static/js/2.4b44c26e.chunk.js"
+  },
+  {
+    "revision": "b69ebe7545e501d0d321",
+    "url": "/popular-film-listings/static/js/main.42431809.chunk.js"
+  },
+  {
+    "revision": "12bfeb9e632b46d65c20",
+    "url": "/popular-film-listings/static/js/runtime~main.50e1f60d.js"
+  }
+]);
